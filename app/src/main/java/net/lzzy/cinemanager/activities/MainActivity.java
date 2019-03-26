fc55fcfb7315d7f3a42ea97d527c6573d100675b
mengchengyu <2586499447@>
2019-03-26 15:50:07 +0800

    自定义标题栏3月26第8节课

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -3,17 +3,59 @@ package net.lzzy.cinemanager.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private View layoutMenu;
+    private SearchView search;
+    private TextView tvTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+
+
+    }
+
+    private void setTitleMenu() {
+        layoutMenu = findViewById(R.id.bar_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_img_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int visible = layoutMenu.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE;
+                layoutMenu.setVisibility(visible);
+            }
+        });
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText("我的订单");
+        search = findViewById(R.id.bar_searchView);
+        findViewById(R.id.bar_order).setOnClickListener(this);
+        findViewById(R.id.bar_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_see_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
